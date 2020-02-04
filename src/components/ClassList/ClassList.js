0b2e73e4e809d1c134b51459300418097a3ae444
Brandon Warner <warner.brandon.a@gmail.com>
2020-02-04 14:45:15 -0600

    Finish assignment

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
     super()
+    this.state = {
+      students: []
+    }
     
+  }
+  
+  componentDidMount(){
+    let classList  = this.props.match.params.class;
+    axios
+    .get(`http://localhost:3005/students?class=${classList}`)
+    .then( res => this.setState({students: res.data}))
+    .catch( err => console.log(err) );
   }
 
   render() {
+    let names = this.state.students.map( (student, index) => {
+        let studentId = student.id;
+        return(
+          <Link key={index} to={`/student/${studentId}`}>
+            <h3>{student.first_name} {student.last_name}</h3>
+          </Link>
+          
+          )
+    } )
     return (
-      <div className="box">
-        <h1></h1>
-        <h2>ClassList:</h2>
+      <div>
+        <div className='subnav'>
+          <Link to='#' className='subnav_links' onClick={this.props.history.goBack}><h3>Back</h3></Link>
+        </div>
+        <div className="box">
+          <h1>{this.props.match.params.class}</h1>
+          <h2>ClassList:</h2>
+          {names}
 
+        </div>
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class Student extends Component {
   constructor() {
-    super()
+    super();
+    this.state = {
+      studentInfo: {}
+    }
 
   }
 
+  componentDidMount(){
+    let { id } = this.props.match.params;
+    axios
+    .get(`http://localhost:3005/students/${id}`)
+    .then( res => this.setState({studentInfo: res.data}) )
+    .catch( err => console.log(err) );
+  }
+
   render() {
+    let { first_name, last_name, grade, email } = this.state.studentInfo;
     return (
-      <div className="box">
-        <h1>Student</h1>
+      <div>
+        <div className='subnav'>
+          <Link to='#' className='subnav_links' onClick={this.props.history.goBack}><h3>Back</h3></Link>
+        </div>
+        <div className="box">
+          <h1>Student</h1>
+          <h1>{ first_name } { last_name }</h1>
+          <h3>Grade: { grade }</h3>
+          <h3>Email: { email }</h3>
+        </div>
       </div>
     )
   }
